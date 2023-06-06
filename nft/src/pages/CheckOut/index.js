f2be85a5409df42d8602519ba20437b554c36308
DOhongson196 <109707579+DOhongson196@users.noreply.github.com>
2023-06-06 23:39:06 +0700

    check out

--- nft/src/pages/CheckOut/index.js
+++ nft/src/pages/CheckOut/index.js
@@ -1,17 +1,26 @@
 import FormCheckOut from './FormCheckOut';
+import MethodPayment from './MethodPayment';
+import { useState } from 'react';
+import InformationOrder from './InformationOrder';
 
 function CheckOut() {
+  const [formValue, setFormValue] = useState({});
+  const [valid, setValid] = useState(false);
   return (
-    <div className="flex flex-col pb-20 bg-[#FFFFFF] dark:bg-bgDarkMode">
+    <div className="flex flex-col pb-20 min-h-[820px] bg-[#FFFFFF] dark:bg-bgDarkMode  text-textColor dark:text-textDarkMode ">
       <div className="flex relative flex-col max-w-screen-xl mx-auto w-full">
-        <div className=" text-textColor  dark:text-textDarkMode mx-auto my-10 text-3xl font-semibold uppercase italic tracking-wider">Incredible</div>
+        <div className="  mx-auto my-10 text-3xl font-semibold uppercase italic tracking-wider">Incredible</div>
+
         <div className="grid grid-cols-2">
-          <div className="col-start border-r border-textColor">
-            <div className=" text-textColor  dark:text-textDarkMode text-xl font-semibold">Delivery information</div>
-            <FormCheckOut />
+          <div className="col-start border-r border-textColor ">
+            {!valid ? (
+              <FormCheckOut setFormValue={setFormValue} setValid={setValid} />
+            ) : (
+              <MethodPayment formValue={formValue} setValid={setValid} />
+            )}
           </div>
-          <div className="col-end ml-12">
-            <div className=" text-textColor  dark:text-textDarkMode text-xl font-semibold">Check payment</div>
+          <div className="col-end ml-12 ">
+            <InformationOrder />
           </div>
         </div>
       </div>
